b769c6cf2320744214fec21d40371fb444349c28
Sam McPhail <sam.mcphail9@gmail.com>
2019-09-17 17:12:03 -0400

    Changed parameters on fade animation and changed nav links to buttons to allow scroll animation later

--- scripts.js
+++ scripts.js
@@ -1,11 +1,30 @@
 $(window).on("load",function() {
   $(window).scroll(function() {
-    $(".scroll-fade").each(function(){
+    $(".scroll-fade-large").each(function(){
       var y = $(this).offset().top - $(window).scrollTop() - $("#nav").height() - $(window).height()/15;
-      var yWithOffset = (y - $(window).height()/5) / ($(window).height() - $(window).height()/3);
+      var yWithOffset = (y - $(window).height()/6) / ($(window).height() - $(window).height()/3);
       var opacity = -(yWithOffset*yWithOffset) + 1;
   	  $(this).css("opacity", opacity);
       console.log($(this).get(0).tagName + " " + y + " " + opacity);
     });
   }).scroll(); //invoke scroll-handler on page-load
+});
+
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    $(".scroll-fade-small").each(function(){
+      var y = $(this).offset().top - $(window).scrollTop() - $("#nav").height();
+      var yWithOffset = (y - $(window).height()/2) / ($(window).height()/7 - $(window).height()/2);
+      var opacity = -(yWithOffset*yWithOffset) + 1;
+  	  $(this).css("opacity", opacity);
+    });
+  }).scroll(); //invoke scroll-handler on page-load
+});
+
+$(document).ready(function (){
+    $("#link").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#div1").offset().top
+        }, 2000);
+    });
 });
