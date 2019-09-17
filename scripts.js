eb5f7e4614121f09b1e00466f0cfadb29cdf0a05
Sam McPhail <sam.mcphail9@gmail.com>
2019-09-17 19:30:27 -0400

    Added function to make navbar transparent when at top, but white otherwise

--- scripts.js
+++ scripts.js
@@ -20,6 +20,32 @@ $(window).on("load",function() {
   }).scroll(); //invoke scroll-handler on page-load
 });
 
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    $("#nav").each(function(){
+      console.log($(window).scrollTop());
+      console.log($(this).css("background-color"));
+      if($(window).scrollTop() != 0){
+        if($(this).css("background-color") === 'rgba(0, 0, 0, 0)'){
+          $(this).animate({
+            backgroundColor: 'rgba(255, 255, 255, 1)'
+          });
+        } else {
+          return;
+        }
+      } else {
+        if($(this).css("background-color") === 'rgba(0, 0, 0, 0)'){
+          return;
+        } else {
+          $(this).animate({
+            backgroundColor: 'rgba(255, 255, 255, 1)'
+          }, 200);
+        }
+      }
+    });
+  }).scroll(); //invoke scroll-handler on page-load
+});
+
 $(document).ready(function (){
     $("#link").click(function (){
         $('html, body').animate({
