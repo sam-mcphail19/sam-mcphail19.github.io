85c4acab731294470ede7c1e0d524d25f3e43bf9
Sam McPhail <sam.mcphail9@gmail.com>
2019-09-17 18:59:00 -0400

    Fixed navbar margin and the spacing in index.html

--- scripts.js
+++ scripts.js
@@ -2,10 +2,9 @@ $(window).on("load",function() {
   $(window).scroll(function() {
     $(".scroll-fade-large").each(function(){
       var y = $(this).offset().top - $(window).scrollTop() - $("#nav").height() - $(window).height()/15;
-      var yWithOffset = (y - $(window).height()/6) / ($(window).height() - $(window).height()/3);
+      var yWithOffset = (y - $(window).height()/6) / ($(window).height() - $(window).height()/2);
       var opacity = -(yWithOffset*yWithOffset) + 1;
   	  $(this).css("opacity", opacity);
-      console.log($(this).get(0).tagName + " " + y + " " + opacity);
     });
   }).scroll(); //invoke scroll-handler on page-load
 });
