cf0cddb020b86156791fa5ac8dab1326cb3a7722
Sam McPhail <sam.mcphail9@gmail.com>
2019-09-25 16:39:34 -0400

    Filled out info and added slide-in entrance animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,8 @@
 $(window).on("load",function() {
   $(window).scroll(function() {
-    $(".scroll-fade-large").each(function(){
+    $(".scroll-fade").each(function(){
       var y = $(this).offset().top - $(window).scrollTop() - $("#nav").height() - $(window).height()/15;
       var yWithOffset = (y - $(window).height()/6) / ($(window).height() - $(window).height()/2);
-      var opacity = -(yWithOffset*yWithOffset) + 1;
-  	  $(this).css("opacity", opacity);
-    });
-  }).scroll(); //invoke scroll-handler on page-load
-});
-
-$(window).on("load",function() {
-  $(window).scroll(function() {
-    $(".scroll-fade-small").each(function(){
-      var y = $(this).offset().top - $(window).scrollTop() - $("#nav").height();
-      var yWithOffset = (y - $(window).height()/2) / ($(window).height()/7 - $(window).height()/2);
       var opacity = -(yWithOffset*yWithOffset) + 1;
   	  $(this).css("opacity", opacity);
     });
@@ -42,4 +31,41 @@ $(document).ready(function (){
             scrollTop: $("#div1").offset().top
         }, 2000);
     });
+});
+
+(function($) {
+  /**
+   * Copyright 2012, Digital Fusion
+   * Licensed under the MIT license.
+   * http://teamdf.com/jquery-plugins/license/
+   *
+   * @author Sam Sehnert
+   * @desc A small plugin that checks whether elements are within
+   *     the user visible viewport of a web browser.
+   *     only accounts for vertical position, not horizontal.
+   */
+  $.fn.visible = function(partial) {
+
+      var $t            = $(this),
+          $w            = $(window),
+          viewTop       = $w.scrollTop(),
+          viewBottom    = viewTop + $w.height(),
+          _top          = $t.offset().top,
+          _bottom       = _top + $t.height()-10,
+          compareTop    = partial === true ? _bottom : _top,
+          compareBottom = partial === true ? _top : _bottom;
+
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+  };
+
+})(jQuery);
+
+$(window).scroll(function(event) {
+  $(".slide-in-right").each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("slide-in-right-animation");
+    }
+  });
 });
