656cee0b7f9a310e818ab2f777020f82e0eda3c1
Sam McPhail <sam.mcphail9@gmail.com>
2019-10-12 10:49:27 -0400

    Added actual headshot image

--- js/scripts.js
+++ js/scripts.js
@@ -26,26 +26,16 @@ $(window).on("load",function() {
 });
 
 (function($) {
-  /**
-   * Copyright 2012, Digital Fusion
-   * Licensed under the MIT license.
-   * http://teamdf.com/jquery-plugins/license/
-   *
-   * @author Sam Sehnert
-   * @desc A small plugin that checks whether elements are within
-   *     the user visible viewport of a web browser.
-   *     only accounts for vertical position, not horizontal.
-   */
   $.fn.visible = function(partial) {
 
-      var $t            = $(this),
-          $w            = $(window),
-          viewTop       = $w.scrollTop(),
-          viewBottom    = viewTop + $w.height(),
-          _top          = $t.offset().top,
-          _bottom       = _top + $t.height()-10,
-          compareTop    = partial === true ? _bottom : _top,
-          compareBottom = partial === true ? _top : _bottom;
+      var t = $(this),
+          w = $(window),
+          viewTop = w.scrollTop(),
+          viewBottom = viewTop + w.height(),
+          top = t.offset().top,
+          bottom = top + t.height()-10,
+          compareTop = partial === true ? bottom : top,
+          compareBottom = partial === true ? top : bottom;
 
     return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
 
